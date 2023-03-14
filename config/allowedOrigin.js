019d22d737ab95944ca34f9186c7196e1800f87a
Gideon <gideons564@gmail.com>
2023-03-14 12:36:21 +0100

    Node api for tech notes

---
+++ config/allowedOrigin.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  "http://localhost:3550, https://www.gidizreapairshop.com",
+  "https://gidizreapairshop.com",
+  "https://www..com/",
+];
+
+module.exports = allowedOrigins;
